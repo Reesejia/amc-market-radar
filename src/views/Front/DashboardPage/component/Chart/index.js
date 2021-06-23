bab2f437ebdd73cdfc9b4af73c384052509f7cde
wjx965558244871638129 <965558244@qq.com>
2021-06-23 16:40:34 +0800

    update

--- src/views/Front/DashboardPage/component/Chart/index.js
+++ src/views/Front/DashboardPage/component/Chart/index.js
@@ -2,22 +2,36 @@ import React, {Component} from 'react';
 import { getBarChart,getLineChart,getPieChart } from "@/views/Front/DashboardPage/Chart";
 import ReactEcharts from 'echarts-for-react';
 export default class ContextPage extends Component {
+  chartRef = React.createRef()
   static getDerivedStateFromProps(nextProps, prevState){
-    const {chartStyle} = nextProps.widget
+    console.log('getDerivedStateFromProps nextProps', nextProps)
+    console.log('getDerivedStateFromProps prevState', prevState)
+    const widget = nextProps && nextProps.widget
+    const {chartStyle} = widget
     let option = {}
     if(chartStyle && chartStyle.chart){
       const {vizDataBase} = chartStyle.chart
       let final = vizDataBase ? `${vizDataBase.replace(/\[ \]/g, '[]').replace(/echarts/g, 'echarts2')}` : ''
       eval(final)
-       return {option}
+       return {option, id: widget.i}
     }
-    return null // 如果返回null，表示不修改状态
+    return null
   }
+
+  // getSnapshotBeforeUpdate(){
+  //   return this.chartRef.current.scrollHeight
+  // }
+
+  componentDidUpdate(prevProps, prevState, scrollHeight){
+    console.log('本次添加的高度', this.chartRef.current.scrollHeight - scrollHeight )
+  }
+
   render() {
     console.log('this.state.optioins', this.state.option)
     return (
       <>
           <ReactEcharts
+            ref={this.chartRef}
             option={this.state.option}
             notMerge={true}
             lazyUpdate={true}
