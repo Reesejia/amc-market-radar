dda335c674d5a7cc08656f4530ad0a56327bb635
zhangyuan <yuanzhang01@newbanker.cn>
2021-07-21 19:11:09 +0800

    redux

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@
 // export default store;
 
 
-import { createStore, applyMiddleware, Store, AnyAction } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
