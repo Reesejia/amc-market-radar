f9b484ad432f9a243ebeb4f7a5b57c039cb23852
wjx965558244871638129 <965558244@qq.com>
2021-07-26 15:15:42 +0800

    change status

--- src/store/reducers/dashboardStore.js
+++ src/store/reducers/dashboardStore.js
@@ -1,6 +1,9 @@
 import { TypeAction } from '@/typings/common';
 import * as types from '../action-types';
 
+
+
+
 let initialState = {
   boardDataOrigin: {},
   boardGridOrigin: {},
@@ -16,9 +19,7 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case types.GET_DASH_ORIGIN_DATA:
       const { originDashId, charsData, dashboard } = payload
-      // state.boardDataOrigin[dashboardId].dashboard = dashboard
-      let o = { ...state, boardDataOrigin: { ...state.boardDataOrigin, [originDashId]: dashboard } };
-      return o
+      return { ...state, boardDataOrigin: { ...state.boardDataOrigin, [originDashId]: dashboard } };
 
     case types.GET_GRID_DATA:
       let { dashboardId, gridPositionData, chartIds } = payload
@@ -26,18 +27,20 @@ export default function (state = initialState, action) {
       const dashGridObj = state.boardGridOrigin[dashboardId]
       dashGridObj.widgets = gridPositionData
       dashGridObj.chartIds = chartIds
-      const d = { ...state, boardGridOrigin: { ...state.boardGridOrigin, [dashboardId]: dashGridObj } }
-      return d
+      return { ...state, boardGridOrigin: { ...state.boardGridOrigin, [dashboardId]: dashGridObj } }
 
     case types.UPDATE_GRIDDATA:
       let { dashId, gridwidgets } = payload
       const dashIdObj = state.boardGridOrigin[dashId]
-      dashIdObj.widgets = gridwidgets
-      const c = { ...state, boardGridOrigin: { ...state.boardGridOrigin, [dashId]: dashIdObj } };
-      return c
 
-      case types.GET_BUSINESS_DATA:
-        return { ...state, chartsData: {...state.chartsData,...payload }};
+      changeStatic(gridwidgets, true)
+      dashIdObj.widgets = gridwidgets
+      console.log(' dashIdObj.widgets',  dashIdObj.widgets)
+      return { ...state, boardGridOrigin: { ...state.boardGridOrigin, [dashId]: dashIdObj } };
+
+
+    case types.GET_BUSINESS_DATA:
+      return { ...state, chartsData: { ...state.chartsData, ...payload } };
 
     case types.GET_NAV_LIST:
       payload.forEach(dashGroup => {
@@ -64,11 +67,23 @@ export default function (state = initialState, action) {
     case types.GROUP_ID:
       return { ...state, groupId: payload, ...state.boardDataOrigin, ...state.boardGridOrigin };
     case types.IS_EDIT_DASHBOARD:
-      return { ...state, isEditDashBoard: payload,...state.boardDataOrigin, ...state.boardGridOrigin};
-      return { ...state, groupId: payload, ...state.boardDataOrigin,  ...state.boardGridOrigin};
-      // case types.UPDATE_ACTIVE_KEY:
-      //   return { ...state, chartsData: {...state.chartsData}}
+      return { ...state, isEditDashBoard: payload, ...state.boardDataOrigin, ...state.boardGridOrigin };
+      return { ...state, groupId: payload, ...state.boardDataOrigin, ...state.boardGridOrigin };
+    // case types.UPDATE_ACTIVE_KEY:
+    //   return { ...state, chartsData: {...state.chartsData}}
     default:
       return state;
   }
+}
+
+
+export function changeStatic(gridwidgets, bool) {
+  gridwidgets.forEach(widget => {
+    widget.static = bool
+    if (widget.subTabs && widget.subTabs.length > 0) {
+      changeStatic(widget.subTabs, bool)
+    } else if (widget.children && widget.children.length > 0) {
+      changeStatic(widget.children, bool)
+    }
+  })
 }
