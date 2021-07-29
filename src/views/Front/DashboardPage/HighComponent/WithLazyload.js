c1db378a1a0a41c4c17cf8b39369b3821342e979
wjx965558244871638129 <965558244@qq.com>
2021-07-29 14:49:36 +0800

    优化 intersection observer

--- src/views/Front/DashboardPage/HighComponent/WithLazyload.js
+++ src/views/Front/DashboardPage/HighComponent/WithLazyload.js
@@ -4,7 +4,7 @@ import { PureComponent } from 'react'
 let tasks = []
 function myNonEssentialWork(deadline) {
   while (deadline.timeRemaining() > 0 && tasks.length > 0) {
-    // console.log('timeRemaining222 item', tasks)
+    // console.log('ssss item', tasks)
     const task = tasks.shift()
     if (!task.classComponent) return
     if (!task.classComponent.state.show) {
@@ -32,11 +32,11 @@ class WithLazyload extends PureComponent {
       let ele = document.getElementById(this.props.id)
       const ob = new IntersectionObserver((changes) => {
         changes.forEach(change => {
-          // console.log('change', change)
           const { isIntersecting, target } = change
-          if (!isIntersecting) {
+          if (isIntersecting) {
             ob.unobserve(ele)
             if (!this.state.show) {
+              // console.log('ssss show')
               this.setState({
                 show: true
               })
