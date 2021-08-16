b9ef3f05b2e2a58561ff6612cbf1e972e272d11d
droden <jianxiwang@newbanker.cn>
2021-08-16 07:20:29 +0000

    Update webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,4 @@
 
-
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
@@ -299,7 +298,6 @@ module.exports = function (webpackEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        maxSize: 2500000,
         chunks: 'all',
         name: isEnvDevelopment,
       },
