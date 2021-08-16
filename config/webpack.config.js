55642edd423f9d0c40ad0c908f0297dc7869ba1b
wjx965558244871638129 <965558244@qq.com>
2021-08-16 14:48:27 +0800

    add opmization chunk maxSize

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -299,6 +299,7 @@ module.exports = function (webpackEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
+        maxSize: 700000,
         chunks: 'all',
         name: isEnvDevelopment,
       },
